6036fe7fc98ebda7470002669d3e7e99db6cc124
ramses-iv <ongerigiddy@gmail.com>
2019-02-05 21:54:39 +0300

    Can reference library artifacts from example code.

--- app/src/main/java/com/interswitchgroup/mobpayexample/MainActivity.java
+++ app/src/main/java/com/interswitchgroup/mobpayexample/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.interswitchgroup.mobpaylib.MobPay;
+import com.interswitchgroup.mobpaylib.model.Invoice;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        MobPay.pay(new Invoice());
     }
 
     @Override
---
+++ mobpaylib/src/main/java/com/interswitchgroup/mobpaylib/MobPay.java
@@ -0,0 +1,8 @@
+package com.interswitchgroup.mobpaylib;
+
+import com.interswitchgroup.mobpaylib.model.Invoice;
+
+public class MobPay {
+    public static void pay(Invoice invoice) {
+    }
+}
