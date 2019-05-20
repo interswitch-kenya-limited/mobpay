9c3a6be8581bf40ddb167e75f6e5733319370f81
ramses-iv <ongerigiddy@gmail.com>
2019-05-20 10:53:25 +0300

    Enabled minification and disabled screenshots as per security recommendations, enapled masking of cvv as a password number field.

--- app/src/main/java/com/interswitchgroup/mobpayexample/MainActivity.java
+++ app/src/main/java/com/interswitchgroup/mobpayexample/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
@@ -51,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
