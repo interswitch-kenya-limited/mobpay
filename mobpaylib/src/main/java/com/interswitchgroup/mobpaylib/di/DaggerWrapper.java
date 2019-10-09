ed5e493491ba660362606ff8d75d1c96b030a761
Allan Mageto <allanbmageto@gmail.com>
2019-10-09 11:51:27 +0300

    Well damn hover is working - ish

--- mobpaylib/src/main/java/com/interswitchgroup/mobpaylib/di/DaggerWrapper.java
+++ mobpaylib/src/main/java/com/interswitchgroup/mobpaylib/di/DaggerWrapper.java
@@ -12,8 +12,8 @@ public class DaggerWrapper {
     }
 
     private static void initComponent(String clientId, String clientSecret) {
-//        String baseurl = "https://testids.interswitch.co.ke:18082/api/v1/";
-        String baseurl = "https://esb.interswitch-ke.com:18082/api/v1/";
+        String baseurl = "https://testids.interswitch.co.ke:18082/api/v1/";
+//        String baseurl = "https://esb.interswitch-ke.com:18082/api/v1/";
         mComponent = DaggerMainComponent
                 .builder()
                 .netModule(new NetModule(baseurl, clientId, clientSecret))
